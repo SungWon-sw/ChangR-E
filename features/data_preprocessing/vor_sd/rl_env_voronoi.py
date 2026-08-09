b72edc68b8f52b38767162feafa5e69f89914eda
Donghyun Kang / 강동현 <luckyandykang@gmail.com>
2026-08-09 21:37:49 +0900

    우옹애

--- features/data_preprocessing/vor_sd/rl_env_voronoi.py
+++ features/data_preprocessing/vor_sd/rl_env_voronoi.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from shapely.geometry import LineString, Polygon
 from shapely.strtree import STRtree
+from scipy.spatial import KDTree # k-D Tree 쓰려고불러옴 
 import sys
 
 # [수정] 차단확률 연산과 보로노이 분할 함수를 코어 모듈(weighted_voronoi.py)에서 직접 가져옵니다.
@@ -56,17 +57,22 @@ class TrafficRLEnv:
         self.tree = STRtree(self.seg_lines)
         print(f"[RL Env] 초기화 완료: 분기점 {self.K}개, 도로 선분 {self.N}개")
 
-    def step(self, weights):
+        neighbor = KDTree(self.site_coords).query(self.site_coords, k=2)[0][:, 1] # neighbor: 가장 가까운 이웃 정점까지의 거리(들의리스트)
+        self.spacing2 = np.median(neighbor) ** 2 # 이거구하는이유) 보로노이어쩌구 식에서 단위가 안 맞고 가중치가 tanh 거쳐서 개작음 -> 값이 바뀔 때 step을 너무많이먹음 + 의미도없음
+    
+    def evaluate(self, weights): # step (였던것)
         """
-        매 에피소드마다 에이전트의 가중치를 받아 멱 다이어그램을 형성하고,
+        매 에피소드마다 에이전트의 가중치를 받아 멱 다이어그램을 형성하고, 
         도로를 물리적으로 절단한 뒤 동적으로 M/G/c/c 차단확률을 계산합니다.
         """
         if len(weights) != self.K:
             raise ValueError(f"가중치 개수({len(weights)})가 분기점 개수({self.K})와 다릅니다.")
             
         # 나눗셈 로직 제거 -> 수학적으로 올바른 가중 보로노이(멱 다이어그램) 코어 함수 직접 호출
-        safe_weights = np.array(weights) 
-        cells = all_power_cells(self.site_coords, safe_weights, self.bbox)
+        # safe_weights = np.array(weights) 
+
+        #  ㄴ 위에 있는거 지움. safe_weights랑 weights랑 다른거없는듯
+        cells = all_power_cells(self.site_coords, weights * self.spacing2 , self.bbox) # weights : 단위통일시킴 + 스케일 키움
         
         stds = np.zeros(self.K)
         
@@ -104,6 +110,20 @@ class TrafficRLEnv:
                 
         mean_std = np.mean(stds)
         return mean_std, stds
+
+    def reset(self):
+        self.weights = np.zeros(self.K)
+        return self.weights.copy() 
+        # 위에저거 
+        # self.weights로만하면
+        # 그 같은 주소라서 나중에 +=로 수정하면 아파질까봐
+        # 걍 copy()로함
+
+    def step(self, action):
+        self.weights += np.array(action) - self.weights.mean()
+        mean_std, stds = self.evaluate(self.weights)
+        reward = -mean_std
+        return self.weights.copy(), reward, False, dict()
 
 # ==============================================================================
 # 학습 루프 사전 테스트 실행부
